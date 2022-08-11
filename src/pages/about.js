6d4fd79a79be76e8f77635e6b901a6aa14cc268c
hachian <jamjam8an@gmail.com>
2022-08-11 23:29:34 +0900

    [new] about, slide, youtube page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function MyReactPage() {
+  return(
+    <Layout title="About">
+      <h1>About Page</h1>
+      <h2>Aboutを書きます</h2>
+    </Layout>
+  )
+}
